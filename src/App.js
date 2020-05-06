96f37e5f3920821849dc24209cbd96a27ce83706
Loi Tran <datloiboi@gmail.com>
2020-05-06 19:27:33 +0700

    Complete slider

--- src/App.js
+++ src/App.js
@@ -1,26 +1,119 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable no-magic-numbers */
+/* eslint-disable no-console */
+/* eslint-disable no-inner-declarations */
+/* eslint-disable no-undef */
+/* eslint no-unused-expressions: 0 */
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import isEmpty from "lodash/isEmpty";
+import isString from "lodash/isString";
+
+import {
+  stateClassroom,
+  createScriptElement,
+  bookingClassroomData,
+} from "./data";
+
+const startSlide = (presInfo, stateSlider, slideAsset, player) => {
+  try {
+
+    PresentationPlayer.start(
+      presInfo,
+      "content",
+      "playerView",
+      function onPlayerInit(player) {
+        player;
+        const preloader = document.getElementById('preloader');
+        preloader.parentNode.removeChild(preloader);
+      },
+      stateSlider,
+      slideAsset
+    );
+  } catch (e) {
+    console.log("error startSlide : ", e);
+  }
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.isLoadedPlayer = false;
+    this.isLoadedMeta = false;
+    this.isLoadedBrowser = false;
+    this.isLoadedBookingClassroom = false;
+    this.slideAsset = null;
+    this.meta = null;
+  }
+
+  componentDidMount() {
+    this.go()
+  }
+
+  go = () => {
+    if (true) {
+      const { learner_next_level } = bookingClassroomData;
+      this.slideAsset = `https://cdn.1on1english.vn/lessons/${learner_next_level}/`;
+      this.meta = this.slideAsset + "meta.js";  
+      this.browsersupport =
+        "https://cdn.1on1english.vn/elearning/assets/js/slide/browsersupport.js";
+
+      createScriptElement(this.meta, (this.isLoadedMeta = true));
+      createScriptElement(this.browsersupport, (this.isLoadedBrowser = true));
+
+      const srcPlayer =
+        "https://cdn.1on1english.vn/elearning/assets/js/slide/player.min.js";
+      createScriptElement(srcPlayer, (this.isLoadedPlayer = true));
+
+      this.isLoadedBookingClassroom = true;
+    }
+    if (
+      window.presInfo &&
+      this.isLoadedMeta &&
+      this.isLoadedBrowser &&
+      this.isLoadedPlayer &&
+      stateClassroom
+    ) {
+      // cross browser for firefox and safari: make sure content id alway empty before trigger new slide
+      const sliderLayer = document.querySelector("#content");
+      sliderLayer.innerHTML = "";
+
+      // fix issue feather api
+      const hasSlider =
+        isString(stateClassroom.slide) && !isEmpty(stateClassroom.slide)
+          ? JSON.parse(stateClassroom.slide)
+          : stateClassroom.slide;
+
+      const stateSlideClassroom = stateClassroom ? hasSlider : null;
+      const slideToolkit = {
+        presInfo,
+        dataSlide: this.slideAsset,
+        stateSlide: stateSlideClassroom,
+      };
+      const slideStateData = {
+        slide: stateSlideClassroom,
+        annotation: "", // set empty temporary
+      };
+      // save slideToolkit to localstorage and use on case socket : `synch slide`
+      localStorage.setItem("slideToolkit", JSON.stringify(slideToolkit));
+      localStorage.setItem("slideStateData", JSON.stringify(slideStateData));
+      // init slide presentation here
+      startSlide(presInfo, stateSlideClassroom, this.slideAsset);
+      // purpose : use `startSlide` one times. avoid state changed trigger start slider
+      this.isLoadedMeta = false;
+      this.isLoadedBrowser = false;
+      this.isLoadedPlayer = false;
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.go}>Start</button>
+        <div id="preloader" />
+        <div id="content"></div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,84 @@
+/* eslint-disable no-undef */
+/* eslint no-unused-expressions: 0 */
+
+// window.presInfo =
+  //   "eNrtPe1y4zaSr8J4a++SKplDAPwAnb1NybZmRhWP7FiazGbjlIuWKJtrmdSSlCfeqama19iqzZ/7cQ82T3LdAEnxS5+2NbJrxoktkQ10N7rRDTQawIcdb2dv54NpEI0a+2T35cFLY1c3dL5rGwfarmmZ9ktqcJuz1sedxk4MwMfBsXJEd99q1q7G4Vm/7uH7nT1u08bO1c6eocHfPsD0g9DtjsNzQg1iGRZAjW939qhGzMbOBMnwon7g357bqqXSc1Oj2vedg3f7COju7H3Y8fHXNf6Cr3E4cRs7zs7er+LFzngMYJH4CiBMohw6o8gVj8Wnj799bEhoPypA6/Ohx0VotqDuOA9NFkCP50Mjm1PgYZEQUwDLlqjWPCoCG/OBb/PAfD7NUR0Zs6HdGjpmQw8L0PYC6IsCtDUf+nZSgF7A5e0gD61r86HjAt2WVt/aAL5zU9Bg7DzRzkd4PsbnTvp8VNaCxk6I6rLzUWj9h51hWuvHFC9WBUS04Q81TEnOzsCJnbMX0cgbuET9BwquX37aF/IcZAgBtdaYUvI7wJ+FzWHshkp85SojN4oCv6G0lb7j752FUIcfe8M7xfEHyiRyldug71xMRk4Ijy6CSawMg2BwFl56t64SutE48CM3UuJA+fzpP4NAuQsmysi7dj9/+kP558SNYg8AzsKJP3DDKMZKEeuN6/ief6kEQ+XKdUbx1d3Zi4mffEwwDDwo7F9OvOhKlBl4w6Ebun7fVS7c+L3r+kq+gCS4VEfsjkaicD8YBSGiwy/y3Zn/Ev7uKUeiBRS0cyj65Gtw8Q+3HwOTEYCHSk9U3wWOExrUM7/6A1V0oIq/jJUovhu5/3O2Mwz8eDfy/uXuETL+/XtBxt6fNPHv+7Odvy6L7S9nL8Z//cv47MXSv6SdBUUCTdRUTSNCCXTV0m1LNwjlumFRi7q7hHz8rYGOQ+jrr78lOi21cJAUlhRrelJa2mrZAzzxCfRTVBFdOWM3Sh51ZX0/yz93yZ9r+Xco/7wPJHBJ462qxtNajaeLNB6F2lMuQ9eNI2hTbFnXv3Quoa09X4luHFQRZ3Stri69paoVonvi0uBVabBaabBlpNHqtE5f/bJ35n/+9L9nMWp7dI1tCEZk6CUWQnRpaMggBMPRAEuE5gJfpEYFhOTeKAeOL0yOKHeO/34484l6Fh84YRjE0ZlP4UsvuHHiwIVvDL41x+ORe+br8HHf8eEHnhvw5cTzXUe+M/Fd6DqDM9/Cyq68/rULHZzDlzeuE5/5Nnz6JbichPCZaALGdSM34SllaU85nJpESR+SqHzr+HcZi8jV2OvHk9D9Linf9KP3brin/OJGaJoHwdkLpRPIjwDx73hpZXWH+sAagrIq/ZETRQB3EYwGoLuJEBLDsrTK319kf4nGjp/hqxJWI1IgEgv9dVVic4qwatG82qxaNlOyVQvmVHLVogUFXrVwpu6rFsx1jlWLpl1p1XLTjreyNky76VpaP+3Ui1S4ttOnSjy7869FVWIqFpE0x5RkvatQxSL7UeiTFfeGHuQxP8tGYJZtW7ZZ8ybzjblXtM5tagV3+0FMoKPbS+2cDAkjF0Oc7lzAG51wFcpwQi1dJ7pmNnSNqLquWZrOwHlrjDQMQ9VsDrNhTeeccpj+6TD7JTozDN3klvGbmCD8Shoa/JDGLrVUQ7dhrm5xRqECBo90lRsWsSyL6UyUCHMlqKGatsY0yyQWgQoblKo2zOY5zBA4M2zGhetOeCBlHphOVZOZjDPNME3kgZmaajAKbBCORNoNCwgwNWJzaCJTgxoX8MAsVWeaxnVKuUEMcyEPzFDhJQBT3eLAOpvPBC0zYRCiAiMMcFkMW7nBLGALhGIaGrM1kxoNHSRjUfhkAW+UcA4wqqlxZlo2sW18UpGFplrMpLZlapYssgsS55ou4Sts4EsLJE2lNgBSYqq2buiarhkUOS+wwSpsgPZotq1zi2miYRuMmSpgM4FyAnSiLGxoqky9AIVpA0TKZcKBBvKilqVRbDmNoVRBppphQL8Aljk3LWI3dmue1ZTcJYBB5yY3DehZjOgGSBhGqRrqNIxPUSX0skAt1aYm9A9gRNeBiwajKgfGqM5Mg5ZbQq+2BOiLpRmiMJLSYBpTiQ2qYxKNoVKlAjWwnU1oONAgDo0nW75elAxUgyL5wDlomw19XsNmZPWCZNTGwBgQ26BcNU2wKzaXkslTb1TsAjCrgapTTqGXcF1vUBO7tQV0c9OWlgKoN7GtbWpLUVPVQAthMUPwx2pZ0IFYUAeOnQgkY6oUOAYiZVfkdXwYYKEsOzE+RIeuaUBLWKZtoP7kGTErjFgMiLJNU4O+ZKAxotAxmZ7rmTWMoFqhHExLdv9lGDFUCu1DdNs0OJq9xYwwldmGTSxuU90wtQIjVoURU1ehi1AwkhT7lt0AJCoYTUIYdC5U4ZkSsShwIxRteYmAllumZVicLCsRHZpKWBk7zwivmGvOVVAqIMlKDAJ8BJQWiAPYAjKhqzHAYduEMTDCVs7l6ODubDvprgVO0E4x6N3EpkySlVpsE0yyYWBfKXMCjo0TkrBJM78DFlizhXHKM2JXGLGp8J0Ws0SDS0bAzOmGxoUrEr4TbB9Ih1Ph3HjDIECUZjL4xoTBShhBUwZNB/oD6mowIFxHU2ZQ4MkAcUjBo8Wre1Ypik4Y+j0xmCW4ByMIzIHZBdsDrQyGzGRVN2yBQoJ14uAhwc80oAE10zQI00yCJk//7WN15p5O0xsLJvKiTMWBx1jZRFQzmDXUEa39QYJGCGTbYmCGHz6KKf8ORoaDLBK9AjHaNhFDt4kYtk3E6NtEjLFNxFgbJYZ+MWJySxlRja/PkMko5BcRBd8mUfBNisLekCgeLxRsV0PBem0oWF9mKaobTMK+u4eLRyeefz0M/MtvlB+9QfTfShc+R8p/Kd04CD03+vzpD1yzOQzcSIlw4QqDK9HYubxy49hTfsCXIsaBLwcApVbB70TcaCnQs3Bad64YRk7SUjJ6khVNS/ZlwHGKpYKkCKn03dB1Rln1hdqTRalO8+f2q2avtad0MajqKFGAf3xc9cLwT26pze/hSp7i+n1oWufSlUHaLBaLBdUzvxldYyDrzD/x+ipRdpUZ7fqD8m2xpb6TRWilSNJIM+DZbBTTktXGTUrraelyGyfYSsiMevC0oSWiIp57h9OXl9CqUb4l5LlqlYn0Vy22SFcWxiDLulEfeVwQa1yR4Nmaugjrl6B2mX6ykO6afrQJ2mf20oV6kWL9dhFksbdvgqlFtmQpYTyALOZG2JePeT/XV7Nj+eMwmRINmJWFurgqlg80kbrGRVBaLihwIiIrVgPm/iUYqlHxBCb4Bre4TSqBFYuViuwSjPKWyhQCK5Ui1RK5yIpW4YRh5AbAs0gpNXTVItyghBBOMLKiU1vVNdPSLMtkIvBrUZVBIU6ZDPJXQkSUYMyXMcbNJIS2y0wVFzYYM0wbw7TlEBEIRCWmaXJLIzJ+Q4mKSw3csKA9NUPLRYHHobnFnFSKVEtUV1megUysZyKTccifTY+nz0a77GfT49mzkQnRnk2XJ+TZ9Hn9+egXfTad3ng+QmHPp9M/n6H9UgNiQ7WZqZlZ5stMoZjJmmlNnstMoRDOiFaXWTBTv0C1CLOJqdGCUIwtZmWWfuVKVHMLnoNQzGciFGDFejadnj8f/eLPptPbz8fTP/U51zI5NHNXd0vBv0dZam6UU2RKuB56cbeR8WZuBN8KS+lkY41dilZ9eeZLQadH1jS6YU2zt03T2AY1jWjbpmqEbFDX9A3rGqHbpmzGJpWNbZ2ybdKFmptWNmPblM3apLKZW6ds1gaVjW9a2fi2KZu9SWXb1CDi0XIfTa2a+2jU5j4ay+Q+vi0cVhEpTugqF85AflOV3pV7J575Qaxc4nZRPBriLpiEykUwuBMA8BYhouDGLZ19EaXZKv7Aw6SZYSj+YELaVRAPgkvA8Nq5uZiEl24YZXiSKhpKHASq0h6KfayuE+N35WbSvyqjaUwpEnlqF67y3nWuVUzbHGPJJFHtBt4A/a7ixZ8//TsS9Y2c2P0G0x7fXQEGLxIJbbH7eyxPGhEZkb30EbyW54+cF/6pWXlkodwGM6oogYk66qRRQTRfZEmy5ivcWo+pgGd+Ew/18OI7zLTEzeinrjMQDSKYnCYH/gQ0iJMKDr2oP4lm0AmsuE4fpC5asBPEbgQ4TpwwVghi6Cl43kmyLR+3c08bFD74uH0ZMKXnLlQaHU+rELmLovGVPFhN2/4A3Aniswrz7Vyoq9ressDC1rxfSmYmhxXzvsr1TIW4zvkDc0S+zpkEcxXkHju412+eqk6uVCG2y+7A7Qehg+mxe+KkoJHnuyIDT+p2ka9yCT8QwDOzCtcqvW7O4JJ9cK2t/ot67PqV1vXvlZOKE2uw3tkKM23HoizJchdI5KauRUaNRVqYvDq1WNuTsDnvaAMZO6AaM5KgMMUNz5pNmMYN3HXdoNxSGTM1w043nusGxV3SjIjILMIsDtRTU9UZbiQ3cfMqbqhPIrzycARaOdyAENVGTnh6OoEMbuvQXIzouNM2H9w2tpWNSoEyfCW9K8cEMSwY2NoWT3dtNxjXVKKZ1E633zdMDSrUliMft9Jr8uSGJRZKOFcNwjXNYBYRZz/MXfMxt4r4PPDc9R32PPTfeg76rz9l/edPU/+Np9zm9tNsc/Mpt3my/vFEGv3ea8Ylr/zIUTGzFtdjxSBLo78NxDyNjeBbQbj6BoXLt415tkHmrQ1rtrFhzba3TbjmBoVb9AlPMphPqsF8szaYby51kMFVMBkNlPeujJZjwNu9dcM7ZeDc/aC8u7oToehm6OK7uzTO/oM8E2B6WmQciPJJ2F4JQhm4z8PV1F8NPIvAGd2TG/2nMd+lUCVH0JZJVYp11JCRACzRFH6z33fHsZtGQzMC24K2sxfJ2ZRzKU3DyJ2gtFiSUKymFM8++jKroYHkRoJw8aahXLh9B88/F22AKxs3DpAyiZTI";
+
+// const stateClassroom = {
+//   booking_code: "B00721",
+//   room_id: "2d1cffc9-fce8-4f28-aa94-6b9eb4561511",
+//   slide:
+//     '{"lastViewedSlide":2,"viewDuration":4,"slideStates":{"0":{"completed":true,"visited":true,"visitedHyperlinks":[]},"1":{"completed":true,"visited":true,"visitedHyperlinks":[]},"2":{"completed":true,"visited":true,"visitedHyperlinks":[]},"3":{"completed":true,"visited":true,"visitedHyperlinks":[]},"4":{"completed":true,"visited":true,"visitedHyperlinks":[]},"5":{"completed":true,"visited":true,"visitedHyperlinks":[]},"6":{"completed":true,"visited":true,"visitedHyperlinks":[]},"7":{"completed":true,"visited":true,"visitedHyperlinks":[]},"8":{"completed":true,"visited":true,"visitedHyperlinks":[]},"9":{"completed":true,"visited":true,"visitedHyperlinks":[]},"10":{"completed":true,"visited":true,"visitedHyperlinks":[]},"11":{"completed":true,"visited":true,"visitedHyperlinks":[]},"12":{"completed":true,"visited":true,"visitedHyperlinks":[]},"13":{"completed":true,"visited":true,"visitedHyperlinks":[]},"14":{"completed":true,"visited":true,"visitedHyperlinks":[]},"15":{"completed":true,"visited":true,"visitedHyperlinks":[]}},"slideTimelineStates":{"0":{"s":0,"t":0.0010000000474974513,"S":{"m":{"s":"completed","t":1.0000000474974513,"T":{"t":[0,1],"at":[{"t":0,"p":[{"time":0,"lL":1588685691493,"freeze":false}]},{"t":0,"p":[{"time":0,"lL":1588685691505,"freeze":false},{"time":1.0000000474974513,"lL":1588740682902,"freeze":false}]}]}},"i":[]}},"1":{"s":0,"t":0.0010000000474974513,"S":{"m":{"s":"completed","t":1.0000000474974513,"T":{"t":[0,1],"at":[{"t":0,"p":[{"time":0,"lL":1588740742443,"freeze":false}]},{"t":0,"p":[{"time":0,"lL":1588740742463,"freeze":false},{"time":1.0000000474974513,"lL":1588744367271,"freeze":false}]}]}},"i":[]}},"2":{"s":0,"t":0.0010000000474974513,"S":{"m":{"s":"playing","t":1.0000000474974513,"T":{"t":[0,1],"at":[{"t":0,"p":[{"time":0,"lL":1588744372240,"freeze":false}]},{"t":0,"p":[{"time":0,"lL":1588744372256,"freeze":false}]}]}},"i":[]}},"3":{"s":0,"t":0.001,"S":{"m":{"s":"waitForTrigger","t":1,"T":{"t":[0,1],"at":[{"t":0,"p":[{"time":0,"lL":1588685690923,"freeze":false}]},{"t":0,"p":[{"time":0,"lL":1588685690959,"freeze":false}]}]},"nt":2},"i":[]}},"4":{"s":0,"t":0.001,"S":{"m":{"s":"waitForTrigger","t":1,"T":{"t":[0,1],"at":[{"t":0,"p":[{"time":0,"lL":1588685690732,"freeze":false}]},{"t":0,"p":[{"time":0,"lL":1588685690756,"freeze":false}]}]},"nt":2},"i":[]}},"5":{"s":0,"t":0.0010000000474974513,"S":{"m":{"s":"completed","t":1.0000000474974513,"T":{"t":[0,1],"at":[{"t":0,"p":[{"time":0,"lL":1588685690538,"freeze":false}]},{"t":0,"p":[{"time":0,"lL":1588685690552,"freeze":false}]}]}},"i":[]}},"6":{"s":0,"t":0.001,"S":{"m":{"s":"waitForTrigger","t":1,"T":{"t":[0,1],"at":[{"t":0,"p":[{"time":0,"lL":1588685690356,"freeze":false}]},{"t":0,"p":[{"time":0,"lL":1588685690365,"freeze":false}]}]},"nt":2},"i":[]}},"7":{"s":0,"t":0.001,"S":{"m":{"s":"waitForTrigger","t":1,"T":{"t":[0,1],"at":[{"t":0,"p":[{"time":0,"lL":1588685690166,"freeze":false}]},{"t":0,"p":[{"time":0,"lL":1588685690176,"freeze":false}]}]},"nt":2},"i":[]}},"8":{"s":0,"t":0.001,"S":{"m":{"s":"waitForTrigger","t":1,"T":{"t":[0,1],"at":[{"t":0,"p":[{"time":0,"lL":1588685689974,"freeze":false}]},{"t":0,"p":[{"time":0,"lL":1588685689983,"freeze":false}]}]},"nt":2},"i":[]}},"9":{"s":0,"t":0.001,"S":{"m":{"s":"waitForTrigger","t":1,"T":{"t":[0,1],"at":[{"t":0,"p":[{"time":0,"lL":1588685689777,"freeze":false}]},{"t":0,"p":[{"time":0,"lL":1588685689799,"freeze":false}]}]},"nt":2},"i":[]}},"10":{"s":0,"t":0.0010000000474974513,"S":{"m":{"s":"completed","t":1.0000000474974513,"T":{"t":[0,1],"at":[{"t":0,"p":[{"time":0,"lL":1588685689590,"freeze":false}]},{"t":0,"p":[{"time":0,"lL":1588685689601,"freeze":false}]}]}},"i":[]}},"11":{"s":0,"t":0.0010000000474974513,"S":{"m":{"s":"completed","t":1.0000000474974513,"T":{"t":[0,1],"at":[{"t":0,"p":[{"time":0,"lL":1588685689390,"freeze":false}]},{"t":0,"p":[{"time":0,"lL":1588685689405,"freeze":false}]}]}},"i":[]}},"12":{"s":0,"t":0.001,"S":{"m":{"s":"waitForTrigger","t":1,"T":{"t":[0,1],"at":[{"t":0,"p":[{"time":0,"lL":1588685689191,"freeze":false}]},{"t":0,"p":[{"time":0,"lL":1588685689204,"freeze":false}]}]},"nt":2},"i":[]}},"13":{"s":0,"t":0.0010000000474974513,"S":{"m":{"s":"completed","t":1.0000000474974513,"T":{"t":[0,1],"at":[{"t":0,"p":[{"time":0,"lL":1588685688992,"freeze":false}]},{"t":0,"p":[{"time":0,"lL":1588685688999,"freeze":false}]}]}},"i":[]}},"14":{"s":0,"t":0.0010000000474974513,"S":{"m":{"s":"completed","t":1.0000000474974513,"T":{"t":[0,1],"at":[{"t":0,"p":[{"time":0,"lL":1588685687216,"freeze":false}]},{"t":0,"p":[{"time":0,"lL":1588685687222,"freeze":false}]}]}},"i":[]}},"15":{"s":0,"t":0.0010000000474974513,"S":{"m":{"s":"completed","t":1.0000000474974513,"T":{"t":[0,1],"at":[{"t":0,"p":[{"time":0,"lL":1588685685666,"freeze":false}]},{"t":0,"p":[{"time":0,"lL":1588685685678,"freeze":false}]}]}},"i":[]}}}}',
+//   annotation: "",
+//   state: "INIT",
+//   created_at: "2020-05-06 05:52:54",
+// };
+
+// const bookingClassroomData = {
+//   code: "B00721",
+//   booking_type: 0,
+//   room_id: "2d1cffc9-fce8-4f28-aa94-6b9eb4561511",
+//   time: "2020-05-08 13:00",
+//   availability_type: 1,
+//   availability_status: "BOOKED",
+//   user_id: 2671,
+//   teacher_name: "Teacher One",
+//   learner_code: "L00354",
+//   learner_name: "Learner One",
+//   learner_next_level: "02010308",
+//   parent_code: "P03163",
+//   lesson_code: null,
+//   feedbacks: [],
+//   reason: null,
+//   status: "INIT",
+// };
+
+window.presInfo =
+  "eNrtPe1y4zaSr8J4a++SKplDAPwAnb1NybZmRhWP7FiazGbjlIuWKJtrmdSSlCfeqama19iqzZ/7cQ82T3LdAEnxS5+2NbJrxoktkQ10N7rRDTQawIcdb2dv54NpEI0a+2T35cFLY1c3dL5rGwfarmmZ9ktqcJuz1sedxk4MwMfBsXJEd99q1q7G4Vm/7uH7nT1u08bO1c6eocHfPsD0g9DtjsNzQg1iGRZAjW939qhGzMbOBMnwon7g357bqqXSc1Oj2vedg3f7COju7H3Y8fHXNf6Cr3E4cRs7zs7er+LFzngMYJH4CiBMohw6o8gVj8Wnj799bEhoPypA6/Ohx0VotqDuOA9NFkCP50Mjm1PgYZEQUwDLlqjWPCoCG/OBb/PAfD7NUR0Zs6HdGjpmQw8L0PYC6IsCtDUf+nZSgF7A5e0gD61r86HjAt2WVt/aAL5zU9Bg7DzRzkd4PsbnTvp8VNaCxk6I6rLzUWj9h51hWuvHFC9WBUS04Q81TEnOzsCJnbMX0cgbuET9BwquX37aF/IcZAgBtdaYUvI7wJ+FzWHshkp85SojN4oCv6G0lb7j752FUIcfe8M7xfEHyiRyldug71xMRk4Ijy6CSawMg2BwFl56t64SutE48CM3UuJA+fzpP4NAuQsmysi7dj9/+kP558SNYg8AzsKJP3DDKMZKEeuN6/ief6kEQ+XKdUbx1d3Zi4mffEwwDDwo7F9OvOhKlBl4w6Ebun7fVS7c+L3r+kq+gCS4VEfsjkaicD8YBSGiwy/y3Zn/Ev7uKUeiBRS0cyj65Gtw8Q+3HwOTEYCHSk9U3wWOExrUM7/6A1V0oIq/jJUovhu5/3O2Mwz8eDfy/uXuETL+/XtBxt6fNPHv+7Odvy6L7S9nL8Z//cv47MXSv6SdBUUCTdRUTSNCCXTV0m1LNwjlumFRi7q7hHz8rYGOQ+jrr78lOi21cJAUlhRrelJa2mrZAzzxCfRTVBFdOWM3Sh51ZX0/yz93yZ9r+Xco/7wPJHBJ462qxtNajaeLNB6F2lMuQ9eNI2hTbFnXv3Quoa09X4luHFQRZ3Stri69paoVonvi0uBVabBaabBlpNHqtE5f/bJ35n/+9L9nMWp7dI1tCEZk6CUWQnRpaMggBMPRAEuE5gJfpEYFhOTeKAeOL0yOKHeO/34484l6Fh84YRjE0ZlP4UsvuHHiwIVvDL41x+ORe+br8HHf8eEHnhvw5cTzXUe+M/Fd6DqDM9/Cyq68/rULHZzDlzeuE5/5Nnz6JbichPCZaALGdSM34SllaU85nJpESR+SqHzr+HcZi8jV2OvHk9D9Linf9KP3brin/OJGaJoHwdkLpRPIjwDx73hpZXWH+sAagrIq/ZETRQB3EYwGoLuJEBLDsrTK319kf4nGjp/hqxJWI1IgEgv9dVVic4qwatG82qxaNlOyVQvmVHLVogUFXrVwpu6rFsx1jlWLpl1p1XLTjreyNky76VpaP+3Ui1S4ttOnSjy7869FVWIqFpE0x5RkvatQxSL7UeiTFfeGHuQxP8tGYJZtW7ZZ8ybzjblXtM5tagV3+0FMoKPbS+2cDAkjF0Oc7lzAG51wFcpwQi1dJ7pmNnSNqLquWZrOwHlrjDQMQ9VsDrNhTeeccpj+6TD7JTozDN3klvGbmCD8Shoa/JDGLrVUQ7dhrm5xRqECBo90lRsWsSyL6UyUCHMlqKGatsY0yyQWgQoblKo2zOY5zBA4M2zGhetOeCBlHphOVZOZjDPNME3kgZmaajAKbBCORNoNCwgwNWJzaCJTgxoX8MAsVWeaxnVKuUEMcyEPzFDhJQBT3eLAOpvPBC0zYRCiAiMMcFkMW7nBLGALhGIaGrM1kxoNHSRjUfhkAW+UcA4wqqlxZlo2sW18UpGFplrMpLZlapYssgsS55ou4Sts4EsLJE2lNgBSYqq2buiarhkUOS+wwSpsgPZotq1zi2miYRuMmSpgM4FyAnSiLGxoqky9AIVpA0TKZcKBBvKilqVRbDmNoVRBppphQL8Aljk3LWI3dmue1ZTcJYBB5yY3DehZjOgGSBhGqRrqNIxPUSX0skAt1aYm9A9gRNeBiwajKgfGqM5Mg5ZbQq+2BOiLpRmiMJLSYBpTiQ2qYxKNoVKlAjWwnU1oONAgDo0nW75elAxUgyL5wDlomw19XsNmZPWCZNTGwBgQ26BcNU2wKzaXkslTb1TsAjCrgapTTqGXcF1vUBO7tQV0c9OWlgKoN7GtbWpLUVPVQAthMUPwx2pZ0IFYUAeOnQgkY6oUOAYiZVfkdXwYYKEsOzE+RIeuaUBLWKZtoP7kGTErjFgMiLJNU4O+ZKAxotAxmZ7rmTWMoFqhHExLdv9lGDFUCu1DdNs0OJq9xYwwldmGTSxuU90wtQIjVoURU1ehi1AwkhT7lt0AJCoYTUIYdC5U4ZkSsShwIxRteYmAllumZVicLCsRHZpKWBk7zwivmGvOVVAqIMlKDAJ8BJQWiAPYAjKhqzHAYduEMTDCVs7l6ODubDvprgVO0E4x6N3EpkySlVpsE0yyYWBfKXMCjo0TkrBJM78DFlizhXHKM2JXGLGp8J0Ws0SDS0bAzOmGxoUrEr4TbB9Ih1Ph3HjDIECUZjL4xoTBShhBUwZNB/oD6mowIFxHU2ZQ4MkAcUjBo8Wre1Ypik4Y+j0xmCW4ByMIzIHZBdsDrQyGzGRVN2yBQoJ14uAhwc80oAE10zQI00yCJk//7WN15p5O0xsLJvKiTMWBx1jZRFQzmDXUEa39QYJGCGTbYmCGHz6KKf8ORoaDLBK9AjHaNhFDt4kYtk3E6NtEjLFNxFgbJYZ+MWJySxlRja/PkMko5BcRBd8mUfBNisLekCgeLxRsV0PBem0oWF9mKaobTMK+u4eLRyeefz0M/MtvlB+9QfTfShc+R8p/Kd04CD03+vzpD1yzOQzcSIlw4QqDK9HYubxy49hTfsCXIsaBLwcApVbB70TcaCnQs3Bad64YRk7SUjJ6khVNS/ZlwHGKpYKkCKn03dB1Rln1hdqTRalO8+f2q2avtad0MajqKFGAf3xc9cLwT26pze/hSp7i+n1oWufSlUHaLBaLBdUzvxldYyDrzD/x+ipRdpUZ7fqD8m2xpb6TRWilSNJIM+DZbBTTktXGTUrraelyGyfYSsiMevC0oSWiIp57h9OXl9CqUb4l5LlqlYn0Vy22SFcWxiDLulEfeVwQa1yR4Nmaugjrl6B2mX6ykO6afrQJ2mf20oV6kWL9dhFksbdvgqlFtmQpYTyALOZG2JePeT/XV7Nj+eMwmRINmJWFurgqlg80kbrGRVBaLihwIiIrVgPm/iUYqlHxBCb4Bre4TSqBFYuViuwSjPKWyhQCK5Ui1RK5yIpW4YRh5AbAs0gpNXTVItyghBBOMLKiU1vVNdPSLMtkIvBrUZVBIU6ZDPJXQkSUYMyXMcbNJIS2y0wVFzYYM0wbw7TlEBEIRCWmaXJLIzJ+Q4mKSw3csKA9NUPLRYHHobnFnFSKVEtUV1megUysZyKTccifTY+nz0a77GfT49mzkQnRnk2XJ+TZ9Hn9+egXfTad3ng+QmHPp9M/n6H9UgNiQ7WZqZlZ5stMoZjJmmlNnstMoRDOiFaXWTBTv0C1CLOJqdGCUIwtZmWWfuVKVHMLnoNQzGciFGDFejadnj8f/eLPptPbz8fTP/U51zI5NHNXd0vBv0dZam6UU2RKuB56cbeR8WZuBN8KS+lkY41dilZ9eeZLQadH1jS6YU2zt03T2AY1jWjbpmqEbFDX9A3rGqHbpmzGJpWNbZ2ybdKFmptWNmPblM3apLKZW6ds1gaVjW9a2fi2KZu9SWXb1CDi0XIfTa2a+2jU5j4ay+Q+vi0cVhEpTugqF85AflOV3pV7J575Qaxc4nZRPBriLpiEykUwuBMA8BYhouDGLZ19EaXZKv7Aw6SZYSj+YELaVRAPgkvA8Nq5uZiEl24YZXiSKhpKHASq0h6KfayuE+N35WbSvyqjaUwpEnlqF67y3nWuVUzbHGPJJFHtBt4A/a7ixZ8//TsS9Y2c2P0G0x7fXQEGLxIJbbH7eyxPGhEZkb30EbyW54+cF/6pWXlkodwGM6oogYk66qRRQTRfZEmy5ivcWo+pgGd+Ew/18OI7zLTEzeinrjMQDSKYnCYH/gQ0iJMKDr2oP4lm0AmsuE4fpC5asBPEbgQ4TpwwVghi6Cl43kmyLR+3c08bFD74uH0ZMKXnLlQaHU+rELmLovGVPFhN2/4A3Aniswrz7Vyoq9ressDC1rxfSmYmhxXzvsr1TIW4zvkDc0S+zpkEcxXkHju412+eqk6uVCG2y+7A7Qehg+mxe+KkoJHnuyIDT+p2ka9yCT8QwDOzCtcqvW7O4JJ9cK2t/ot67PqV1vXvlZOKE2uw3tkKM23HoizJchdI5KauRUaNRVqYvDq1WNuTsDnvaAMZO6AaM5KgMMUNz5pNmMYN3HXdoNxSGTM1w043nusGxV3SjIjILMIsDtRTU9UZbiQ3cfMqbqhPIrzycARaOdyAENVGTnh6OoEMbuvQXIzouNM2H9w2tpWNSoEyfCW9K8cEMSwY2NoWT3dtNxjXVKKZ1E633zdMDSrUliMft9Jr8uSGJRZKOFcNwjXNYBYRZz/MXfMxt4r4PPDc9R32PPTfeg76rz9l/edPU/+Np9zm9tNsc/Mpt3my/vFEGv3ea8Ylr/zIUTGzFtdjxSBLo78NxDyNjeBbQbj6BoXLt415tkHmrQ1rtrFhzba3TbjmBoVb9AlPMphPqsF8szaYby51kMFVMBkNlPeujJZjwNu9dcM7ZeDc/aC8u7oToehm6OK7uzTO/oM8E2B6WmQciPJJ2F4JQhm4z8PV1F8NPIvAGd2TG/2nMd+lUCVH0JZJVYp11JCRACzRFH6z33fHsZtGQzMC24K2sxfJ2ZRzKU3DyJ2gtFiSUKymFM8++jKroYHkRoJw8aahXLh9B88/F22AKxs3DpAyiZTI";
+
+const stateClassroom = {
+  booking_code: "B00737",
+  room_id: "15076b24-d10b-4df4-80c6-d22ccc1878bb",
+  slide: "",
+  annotation: "",
+  state: "INIT",
+  created_at: "2020-05-06 07:31:57",
+};
+
+const bookingClassroomData = {
+  code: "B00737",
+  booking_type: 0,
+  room_id: "15076b24-d10b-4df4-80c6-d22ccc1878bb",
+  time: "2020-05-23 11:30",
+  availability_type: 1,
+  availability_status: "BOOKED",
+  user_id: 2675,
+  teacher_name: "Teacher Five",
+  learner_code: "L00361",
+  learner_name: "Learner Five",
+  learner_next_level: "02020708",
+  parent_code: "P03167",
+  lesson_code: null,
+  feedbacks: [],
+  reason: null,
+  status: "INIT",
+};
+
+const createScriptElement = (src, callbackActions) => {
+  const script = document.createElement("script");
+  script.async = false;
+  script.src = src;
+  if (callbackActions) {
+    script.onload = () => {
+      // purpose : need player.js loaded to trigger global variable like `presInfo` and `player`
+      callbackActions;
+    };
+  }
+  document.getElementsByTagName("body")[0].appendChild(script);
+};
+
+export {
+  bookingClassroomData,
+  stateClassroom,
+  createScriptElement,
+};
